9304bcb0b3c1774dd1050de8f5387703aac7a50d
Yago Garcia Alonso <ygarciaa03@instituto.extremadura.es>
2025-09-26 17:01:31 +0200

    Ej_Finales_3

--- Pruebasinicio/DWEC/Ej-Finales_8.js
+++ Pruebasinicio/DWEC/Ej-Finales_8.js
@@ -1,17 +1,23 @@
 function procesarArray(arr) {
-if (!arr.every(elem => typeof elem === "number")) {
-    alert("Error")
-    }
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = arr[i] * 2;
-    }
-    if (todosPares) {
-    alert("Exito");
+  if (!arr.every(elem => typeof elem === "number")) {
+    alert("Error");
+    return;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i] * 2;
+  }
+
+  const todosPares = arr.every(num => num % 2 === 0);
+
+  if (todosPares) {
+    alert("Éxito");
   } else {
     alert("Error");
   }
 }
-procesarArray([1, 2, 3]);     
+
+procesarArray([1, 2, 3]);
 procesarArray([1, "a", 3]);
-procesarArray([2, 5, 7]);  
-procesarArray([2.5,3,4])  
+procesarArray([2, 5, 7]);
+procesarArray([2.5, 3, 4]);   
