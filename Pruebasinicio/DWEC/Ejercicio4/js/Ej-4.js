b9514982bc44099d81746c6aa32bcd1f8c69761c
Yago Garcia Alonso <ygarciaa03@instituto.extremadura.es>
2025-10-09 17:22:33 +0200

    Ejercicio4BOMyDOM

---
+++ Pruebasinicio/DWEC/Ejercicio4/js/Ej-4.js
@@ -0,0 +1,9 @@
+const div = document.querySelector('div')
+    if(div){
+        div.style.boxSizing = "border-box";
+        div.style.maxWidth = "200PX";
+        div.style.padding = "50px";
+        div.style.color = "white";
+        div.style.background = "red";
+    }
+
