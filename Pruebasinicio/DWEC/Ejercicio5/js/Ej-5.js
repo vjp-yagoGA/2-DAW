777b4184b1b5ea03caa6ab9b53f8e2db20b23212
Yago Garcia Alonso <ygarciaa03@instituto.extremadura.es>
2025-10-09 18:01:43 +0200

    Ejercicio5

---
+++ Pruebasinicio/DWEC/Ejercicio5/js/Ej-5.js
@@ -0,0 +1,22 @@
+const div = document.querySelector('#ContenedorGeneral');
+if (div) {
+    div.style.boxSizing = "border-box";
+    div.style.height = "200px";
+    div.style.width = "200px";
+    div.style.border = "1px solid red";
+    div.style.color = "blue";
+    div.style.background = "#9e9e9e";
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const div = document.getElementById('ContenedorGeneral');
+    function actualizarDimensiones() {
+        div.textContent = `${window.innerWidth} - ${window.innerHeight}`;
+    }
+
+    actualizarDimensiones();
+
+    window.addEventListener('resize', actualizarDimensiones);
+});
+    
+
