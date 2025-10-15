edb125ec8f7165a38c881b5cefd45597a11b594b
Yago Garcia Alonso <ygarciaa03@instituto.extremadura.es>
2025-10-15 20:07:05 +0200

    Ej-401_Opcional_1.1

--- Pruebasinicio/DWES/Ej-401_Opcional.php
+++ Pruebasinicio/DWES/Ej-401_Opcional.php
@@ -1,17 +1,44 @@
 <?php
-ob_start();
-
-$get_params = !empty($_GET) ? $_GET : null;
-$post_params = !empty($_POST) ? $_POST : null;
+$get_params = $_GET ?: null;
+$post_params = $_POST ?: null;
 $server_data = $_SERVER;
-
-
-include 'Ej-401_Opcional.html';
-
-ob_end_flush();
 ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Ej-401 Opcional</title>
+</head>
+<body>
+    <h1>Ej-401 Opcional</h1>
+    <div class="enlaces">
+        <a href="Ej-401_enlace.html">Ir a Enlace</a> |
+        <a href="Ej-401_post.html">Ir a Formulario</a>
+    </div>
 
+    <?php
+    if ($get_params) {
+        echo "<h2>Parámetros GET recibidos:</h2>";
+        echo "<pre>" . print_r($get_params, true) . "</pre>";
+    }
 
+    if ($post_params) {
+        echo "<h2>Parámetros POST recibidos:</h2>";
+        echo "<pre>" . print_r($post_params, true) . "</pre>";
+    }
+    echo "<h2>HTTP_REFERER:</h2>";
+    echo isset($server_data['HTTP_REFERER']) ? htmlspecialchars($server_data['HTTP_REFERER']) : "No proporcionado";
+    echo "<h2>Contenido de \$_SERVER:</h2>";
+    echo "<tr><th>Clave</th><th>Valor</th></tr>";
+    foreach ($server_data as $key => $value) {
+        echo "<tr>";
+        echo "<td>" . htmlspecialchars($key) . "</td>";
+        echo "<td>" . htmlspecialchars(is_array($value) ? print_r($value, true) : $value) . "</td>";
+        echo "</tr>";
+    }
+    ?>
+</body>
+</html>
 
 
 
